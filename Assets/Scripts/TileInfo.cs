992dace1f4579fccfcfacb8da026d0a821c24ab6
nico188f <35289468+nico188f@users.noreply.github.com>
2023-03-22 15:14:22 +0100

    tileinfo start

---
+++ Assets/Scripts/TileInfo.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileInfo : MonoBehaviour
+{
+    public int id;
+    public List<int> NeighborIds = new List<int>();
+    public List<int> LegalNeighborIds = new List<int>();
+
+
+
+
+}
