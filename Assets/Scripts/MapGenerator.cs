17a1463adff430a399211cb62daf0fc597b5bc94
L3fty69 <83811912+L3fty69@users.noreply.github.com>
2023-03-22 10:49:30 +0100

    mapTiles update

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    public GameObject mapTile;
+    public GameObject MapTile;
 
     [SerializeField] private int mapWidth;
     [SerializeField] private int mapHight;
 
+    static public List<GameObject> mapTiles = new List<GameObject>();
+    private List<GameObject> pathTiles = new List<GameObject>();
+
+
     private void Start()
     {
         generateMap();
+    }
+
+    private List<GameObject> getTopEdgeTiles()
+    {
+        List<GameObject> edgeTiles =new List<GameObject>();
+
+        for (int i = mapWidth * (mapHight-1); i < mapWidth * mapHight; i++)
+        {
+            edgeTiles.Add(mapTiles[i]);
+        }
+
+        return edgeTiles;
+    }
+
+    private List<GameObject> getBottomEdgeTiles()
+    {
+        List<GameObject> edgeTiles = new List<GameObject>();
+
+        return edgeTiles;
     }
 
     private void generateMap()
@@ -20,10 +43,16 @@ public class MapGenerator : MonoBehaviour
         {
             for (int x = 0; x < mapWidth; x++)
             {
-                GameObject newTile = Instantiate(mapTile);
+                GameObject newTile = Instantiate(MapTile);
+
+                mapTiles.Add(newTile);
 
                 newTile.transform.position = new Vector2(x, y);
             }
-        }
+        } 
+
+        List<GameObject> topEdgeTiles = getTopEdgeTiles();
+
+        Destroy(topEdgeTiles[0]);
     }
 }
