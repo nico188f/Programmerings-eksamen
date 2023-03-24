5fbce30b4beaf85096ae3e4a96c1933a88b1c477
nico188f <35289468+nico188f@users.noreply.github.com>
2023-03-24 13:04:29 +0100

    Update TowerScript.cs

--- Assets/Scripts/TowerScript.cs
+++ Assets/Scripts/TowerScript.cs
@@ -1,13 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class TowerScript : MonoBehaviour
 {
     public Tower TowerType;
 
+    public float timeSinceLastShoot;
+    public List<GameObject> EnemysInRange = new List<GameObject>();
+
     private void Start()
     {
         GetComponent<SpriteRenderer>().color = TowerType.color;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("enemy"))
+        {
+            EnemysInRange.Add(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("enemy"))
+        {
+            EnemysInRange.Remove(collision.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        timeSinceLastShoot += Time.deltaTime;
+        if (timeSinceLastShoot > TowerType.secondsBetweenShoots && EnemysInRange.Count > 0)
+        {
+            //ShootEnemy(TargetEnemy)
+        }
+    }
+
+    public void ShootEnemy(GameObject Enemy)
+    {
+        
+    }
+
+    /*public Vector2 TryToAim(Vector2 PointA, Vector2 PointB, GameObject Enemy)
+    {
+        float test = 
+    }*/
+
+    
+
 }
